cc0ee905c155739d36f8e959504bd7858806cbb6
Karina <psinka.94@gmail.com>
2024-06-02 21:08:36 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+course_name = 'Python'
+time_spent_on_one_homework = hours_count / homework_count
+print('Курс: ',course_name, ', ', 'всего задач: ', homework_count, ', ', 'затрачено часов: ', hours_count, ', ', 'среднее время выполнения: ', time_spent_on_one_homework, ' часа.')
